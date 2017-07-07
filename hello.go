2d8e2a5ad2dc5359195ea8d65c7ca957ddda4f5c
pochi <pochi.black@gmail.com>
2017-07-07 11:01:34 +0900

    get file list in specific dir

--- hello.go
+++ hello.go
@@ -6,9 +6,21 @@ package main
 */
 import "C"
 import "unsafe"
+import "fmt"
+import "io/ioutil"
 
+/*
+  - TODO 1: loop for specific dir - 
+  TODO 2: load image and transfer to c program
+*/
 func main() {
 	str := C.CString("Hello,  cgo")
 	defer C.free(unsafe.Pointer(str))
 	C.hello(str)
+
+	dir := "./images"
+	files, _ := ioutil.ReadDir(dir)
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
 }
